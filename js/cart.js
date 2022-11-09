da8745a3246b33f485d1da7c86dfd217a43587b0
AlejoFernandez10 <alejofernandez1239@gmail.com>
2022-11-08 23:04:10 -0300

    Segunda entrega proyecto

--- js/cart.js
+++ js/cart.js
@@ -1,16 +1,56 @@
+const resumenPedido = [];
+const precioTotal = [];
+
+const cafeDisponible = {
+    capsula:{
+        nombre:"capsula",
+        1:"Ristretto",
+        2:"Espresso intenso",
+        3:"Caffé Crema",
+        4:"Capuccino",
+        5:"Latte Macchiato",
+        precio: 1500,
+    },
+    filtro:{
+        nombre:"filtro",
+        1:"Inmerso",
+        2:"Vacío",
+        3:"Cocido",
+        4:"Goteo",
+        precio: 1000,
+    },
+    espresso:{
+        nombre:"espresso",
+        1:"Ristretto",
+        2:"Espresso intenso",
+        3:"Caffé Crema",
+        4:"Capuccino",
+        5:"Latte Macchiato",
+        precio: 1200,
+    }
+
+}
+
+const cantidadesPrecio = {
+    "250g":500,  
+    "500g":900,  
+    "1000g":1700, 
+}
+
 
 // FUNCIONES
 
-const tiposDeCafeCapsula = () =>{
+const tiposDeCafe = () =>{
     let preguntaTiposDeCafe=  prompt(`Elija su tipo de cafe:
     1-Ristretto
     2-Espresso intenso
     3-Caffé Crema
     4-Capuccino
     5-Latte Macchiato
-    `);
 
+    `);    
 
+    //VALIDACION
     while(preguntaTiposDeCafe < 1 || preguntaTiposDeCafe > 5){
 
         let preguntaValidacion = prompt(`Por favor introduzca un valor válido:
@@ -24,19 +64,38 @@ const tiposDeCafeCapsula = () =>{
             break
         }
     }
-    return preguntaTiposDeCafe
+
+
+    if(preguntaTiposDeCafe == `1` ||preguntaTiposDeCafe == `Ristretto` ||preguntaTiposDeCafe == `ristretto`){
+        return resumenPedido.push(cafeDisponible.capsula[1]);
+    }
+    else if(preguntaTiposDeCafe == `2` ||preguntaTiposDeCafe == `Espresso intenso` ||preguntaTiposDeCafe == `espresso intenso`){
+        return resumenPedido.push(cafeDisponible.capsula[2]);
+    }
+    else if(preguntaTiposDeCafe == `3` ||preguntaTiposDeCafe == `Caffé Crema` ||preguntaTiposDeCafe == `caffé crema`){
+        return resumenPedido.push(cafeDisponible.capsula[3]);
+    }
+    else if(preguntaTiposDeCafe == `4` ||preguntaTiposDeCafe == `Capuccino` ||preguntaTiposDeCafe == `capuccino`){
+        return resumenPedido.push(cafeDisponible.capsula[4]);
+    }
+    else if(preguntaTiposDeCafe == `5` ||preguntaTiposDeCafe == `Latte Macchiato` ||preguntaTiposDeCafe == `latte macchiato`){
+        return resumenPedido.push(cafeDisponible.capsula[5]);
+    }
 }
 
+
 const tiposDeCafeFiltro = () =>{
-    let preguntaTiposDeCafe=  prompt(`De que forma desea que su café sea filtrado:
+    let preguntaTiposDeCafeFiltro=  prompt(`De que forma desea que su café sea filtrado:
     1-Inmerso
     2-Vacío
     3-Cocido
     4-Goteo
     5-Presión
-    `);
 
-    while(preguntaTiposDeCafe < 1 || preguntaTiposDeCafe > 5){
+    `);
+    
+
+    while(preguntaTiposDeCafeFiltro < 1 || preguntaTiposDeCafeFiltro > 5){
 
         let preguntaValidacion = prompt(`Por favor introduzca un valor válido:
         1-Inmerso
@@ -44,36 +103,31 @@ const tiposDeCafeFiltro = () =>{
         3-Cocido
         4-Goteo
         5-Presión
-        `)
+
+        `);
         if (preguntaValidacion >= 1 && preguntaValidacion <= 5 ){
             break
         }
     }
-    return preguntaTiposDeCafe
-}
 
-const tiposDeCafeEspresso = () =>{
-    let preguntaTiposDeCafe=  prompt(`Como desea su Espresso:
-    1-Romano
-    2-Machiatto
-    3-Ristretto
-    4-Coretto
-    `);
-
-    while(preguntaTiposDeCafe < 1 || preguntaTiposDeCafe > 4){
-
-        let preguntaValidacion = prompt(`Por favor introduzca un valor válido:
-        1-Romano
-        2-Machiatto
-        3-Ristretto
-        4-Coretto
-        `)
-        if (preguntaValidacion >= 1 && preguntaValidacion <= 4 ){
-            break
-        }
+    if(preguntaTiposDeCafeFiltro == `1` || preguntaTiposDeCafeFiltro == `Inmerso` || preguntaTiposDeCafeFiltro == `inmerso`){
+        return resumenPedido.push(cafeDisponible.filtro[1]);
     }
-    return preguntaTiposDeCafe
+    else if(preguntaTiposDeCafeFiltro == `2` || preguntaTiposDeCafeFiltro == `Vacio` || preguntaTiposDeCafeFiltro == `vacio`){
+        return resumenPedido.push(cafeDisponible.filtro[2]);
+    }
+    else if(preguntaTiposDeCafeFiltro == `3` || preguntaTiposDeCafeFiltro == `Cocido` || preguntaTiposDeCafeFiltro == `cocido`){
+        return resumenPedido.push(cafeDisponible.filtro[3]);
+    }
+    else if(preguntaTiposDeCafeFiltro == `4` || preguntaTiposDeCafeFiltro == `Goteo` || preguntaTiposDeCafeFiltro == `goteo`){
+        return resumenPedido.push(cafeDisponible.filtro[4]);
+    }
+    else if(preguntaTiposDeCafeFiltro == `5` || preguntaTiposDeCafeFiltro == `Presion` || preguntaTiposDeCafeFiltro == `presion`){
+        return resumenPedido.push(cafeDisponible.filtro[5]);
+    }
 }
+
+
 
 
 const cantidad = () =>{
@@ -83,50 +137,118 @@ const cantidad = () =>{
     2-500g
     3-1000g
     `);
-
-    return cantidadRespuesta
+    
+    if(cantidadRespuesta == `1` || cantidadRespuesta == `250g`){
+        precioTotal.push(cantidadesPrecio["250g"])
+        return resumenPedido.push(`250g`);
+    }
+    else if(cantidadRespuesta == `2` || cantidadRespuesta == `500g`){
+        precioTotal.push(cantidadesPrecio["500g"])
+        return resumenPedido.push(`500g`);
+    }
+    else if(cantidadRespuesta == `3` || cantidadRespuesta == `1000g`){
+        precioTotal.push(cantidadesPrecio["1000g"])
+        return resumenPedido.push(`1000g`);
+    }  
+    
 }
 
 
 
 // PREGUNTAS
 
-const comoTomaCafe = prompt(`Como toma su cafe?:
+const comoTomaCafePregunta = prompt(`Como toma su cafe?:
 1-Capsula
 2-Filtro
 3-Espresso
+
 `);
 
+
+
+if(comoTomaCafePregunta == `1` || comoTomaCafePregunta == `Capsula` || comoTomaCafePregunta == `capsula`){
+     resumenPedido.push(cafeDisponible.capsula.nombre);
+     precioTotal.push(cafeDisponible.capsula.precio)
+     tiposDeCafe();
+     cantidad();
+     alert(`Su pedido ya esta siendo elaborado!!`)
+}
+else if(comoTomaCafePregunta == `2` || comoTomaCafePregunta == `Filtro` || comoTomaCafePregunta == `filtro`){
+     resumenPedido.push(cafeDisponible.filtro.nombre);
+     precioTotal.push(cafeDisponible.filtro.precio)
+     tiposDeCafeFiltro();
+     cantidad();
+     alert(`Su pedido ya esta siendo elaborado!!`)
+}
+else if(comoTomaCafePregunta == `3` || comoTomaCafePregunta == `Espresso` || comoTomaCafePregunta == `espresso`){
+     resumenPedido.push(cafeDisponible.espresso.nombre);
+     precioTotal.push(cafeDisponible.espresso.precio)
+     tiposDeCafe();
+     cantidad();
+     alert(`Su pedido ya esta siendo elaborado!!`)
+};
+
+
+
 //VALIDACION Pregunta 1
-while(comoTomaCafe < 1 || comoTomaCafe > 3){
+while(comoTomaCafePregunta < 1 || comoTomaCafePregunta > 3 ){
 
     let preguntaValidacion = prompt(`Por favor introduzca una opcion válida:
     1-Capsula
     2-Filtro
-    3-Espresso  
-    `)
-    if (preguntaValidacion >= 1 && preguntaValidacion <= 3 ){
+    3-Espresso
+    
+    `);
+
+    if (preguntaValidacion >= 1 && preguntaValidacion <= 3  ){
+
+        if(preguntaValidacion == `1` || preguntaValidacion == `Capsula` || preguntaValidacion == `capsula`){
+            resumenPedido.push(cafeDisponible.capsula.nombre);
+            tiposDeCafe();
+            cantidad();
+            alert(`Su pedido ya esta siendo elaborado!!`)
+       }
+
+       else if(preguntaValidacion == `2` || preguntaValidacion == `Filtro` || preguntaValidacion == `filtro`){
+            resumenPedido.push(cafeDisponible.filtro.nombre);
+            tiposDeCafeFiltro();
+            cantidad();
+            alert(`Su pedido ya esta siendo elaborado!!`)
+       }
+
+       else if(preguntaValidacion == `3` || preguntaValidacion == `Espresso` || preguntaValidacion == `espresso`){
+            resumenPedido.push(cafeDisponible.capsula.nombre);
+            tiposDeCafe();
+            cantidad();
+            alert(`Su pedido ya esta siendo elaborado!!`)
+       }  
         break
     }
 }
 
 
-if (comoTomaCafe == "1" || comoTomaCafe == `Capsula` || comoTomaCafe == `capsula`){
-    tiposDeCafeCapsula();
-    cantidad();
-    alert(`Su pedido ya esta siendo elaborado!!`)
-}
-else if((comoTomaCafe == "2" || comoTomaCafe == `filtro` || comoTomaCafe == `Filtro`)){
-    tiposDeCafeFiltro();
-    cantidad();
-    alert(`Su pedido ya esta siendo elaborado!!`)
-}
-else{
-    tiposDeCafeEspresso();
-    cantidad();
-    alert(`Su pedido ya esta siendo elaborado!!`)
-}
+//PRECIO TOTAL
+
+const precioFinalDelPedido = precioTotal.reduce(
+    (sumaPrecios, preciosIndividuales) => sumaPrecios + preciosIndividuales,
+    0
+)
+
+//Enviamos precio al resumen
+resumenPedido.push(`Total: $${precioFinalDelPedido}`);
 
 
+resumenPedido.forEach(e => {
+
+    const pedido = document.getElementById(`pedido`);
+    const createDiv = document.createElement(`div`);
+
+    createDiv.innerHTML = `    
+    <p>${e}</p>
+
+    `
+
+    pedido.appendChild(createDiv);
+}) 
 
 
